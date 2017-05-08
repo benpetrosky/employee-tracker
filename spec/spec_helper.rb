6e18203d545042a39decea41db7fe1121a44ceb5
Ben Petrosky and Jesse Hoare Evans <ben+jesse@>
2017-05-08 15:36:24 -0700

    add initial files

---
+++ spec/spec_helper.rb
@@ -0,0 +1,12 @@
+ENV['RACK_ENV'] = 'test'
+
+require('rspec')
+require("pg")
+require("sinatra/activerecord")
+require("division")
+require("employee")
+
+RSpec.configure do |config|
+  config.after(:each) do
+  end
+end
